31ad511ec6314281cbeae961bca0468db4edace3
rafael-malmegrin <104175901+rafael-malmegrin@users.noreply.github.com>
2022-07-29 11:58:19 -0300

    Add files via upload

--- keyboards/40percentclub/gherkin/keymaps/rafael_caps/keymap.c
+++ keyboards/40percentclub/gherkin/keymaps/rafael_caps/keymap.c
@@ -38,14 +38,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [1] = LAYOUT_ortho_3x10(
     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,
-    KC_LSFT,    KC_LCTL,    KC_LALT,    KC_LGUI,    KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,
+    KC_LSFT,    KC_LCTL,    KC_LALT,    KC_LGUI,    KC_NO,      KC_NO,      KC_TAB,     KC_NO,      KC_NO,      KC_NO,
     KC_CAPS,    KC_NO,      KC_NO,      KC_NO,      KC_SPC,     KC_BSPC,    KC_TRNS,    KC_NO,      KC_NO,      KC_NO
   ),
 
   [2] = LAYOUT_ortho_3x10(
     KC_GRV,     KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_MINS,    KC_EQL,     KC_LBRC,    KC_RBRC,    KC_SCLN,
     KC_LSFT,    KC_LCTL,    KC_LALT,    KC_LGUI,    KC_NO,      KC_BSLS,    KC_COMM,    KC_DOT,     KC_SLSH,    KC_QUOT,
-    KC_TAB,     KC_NO,      KC_NO,      KC_TRNS,    KC_SPC,     KC_DEL,     KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT
+    KC_NO,      KC_NO,      KC_NO,      KC_TRNS,    KC_SPC,     KC_DEL,     KC_LEFT,    KC_DOWN,    KC_UP,      KC_RGHT
   ),
 
   [3] = LAYOUT_ortho_3x10(
